b6a1243e56ef5a46cee06b44dc672f3ec700e775
Jed Malveda <jedmalveda@gmail.com>
2022-07-08 11:09:32 +0800

    Added login and registration functions.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,20 +6,33 @@
 
 @section('content')
     <div class="login-box">
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         <!-- /.login-logo -->
         <div class="card card-outline card-primary">
             <div class="card-header text-center">
                 <a href="#" class="h1">{{ config('app.name', 'Laravel') }}</a>
             </div>
             <div class="card-body">
-                <form action="#" method="post">
+                <form action="{{ route('login') }}" method="post">
+                    @csrf
                     <div class="form-group mb-3">
                         <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email">
+                        <input type="email" class="form-control" name="email">
                     </div>
                     <div class="form-group mb-4">
                         <label for="email">Password</label>
-                        <input type="password" class="form-control" id="password">
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                    <div class="form-group mb-4">
+                        <a href="{{ route('register') }}">Register an Account</a>
                     </div>
                     <div class="row">
                         <div class="col-12">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,49 +1,68 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@section('custom_css')
+    {{-- Insert custom css here --}}
+@endsection
 
 @section('content')
-    @if ($errors->any())
-        <div>
-            <div>{{ __('Whoops! Something went wrong.') }}</div>
+    <div class="login-box">
+        @if ($errors->any())
+            <div>
+                <div>{{ __('Whoops! Something went wrong.') }}</div>
 
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+    <!-- /.login-logo -->
+        <div class="card card-outline card-primary">
+            <div class="card-header text-center">
+                <a href="#" class="h1">{{ config('app.name', 'Laravel') }}</a>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('register') }}" method="post">
+                    @csrf
+                    <div class="form-group mb-4">
+                        <label>{{ __('Name') }}</label>
+                        <input class="form-control" type="text" name="name" value="{{ old('name') }}" required autofocus autocomplete="name" />
+                    </div>
+
+                    <div class="form-group mb-4">
+                        <label>{{ __('Email') }}</label>
+                        <input class="form-control" type="email" name="email" value="{{ old('email') }}" required />
+                    </div>
+
+                    <div class="form-group mb-4">
+                        <label>{{ __('Password') }}</label>
+                        <input class="form-control" type="password" name="password" required autocomplete="new-password" />
+                    </div>
+
+                    <div class="form-group mb-4">
+                        <label>{{ __('Confirm Password') }}</label>
+                        <input class="form-control" type="password" name="password_confirmation" required autocomplete="new-password" />
+                    </div>
+
+                    <a href="{{ route('login') }}">
+                        {{ __('Already registered?') }}
+                    </a>
+
+                    <div>
+                        <button type="submit">
+                            {{ __('Register') }}
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <!-- /.card-body -->
         </div>
-    @endif
+        <!-- /.card -->
+    </div>
+@endsection
 
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-
-        <div>
-            <label>{{ __('Name') }}</label>
-            <input type="text" name="name" value="{{ old('name') }}" required autofocus autocomplete="name" />
-        </div>
-
-        <div>
-            <label>{{ __('Email') }}</label>
-            <input type="email" name="email" value="{{ old('email') }}" required />
-        </div>
-
-        <div>
-            <label>{{ __('Password') }}</label>
-            <input type="password" name="password" required autocomplete="new-password" />
-        </div>
-
-        <div>
-            <label>{{ __('Confirm Password') }}</label>
-            <input type="password" name="password_confirmation" required autocomplete="new-password" />
-        </div>
-
-        <a href="{{ route('login') }}">
-            {{ __('Already registered?') }}
-        </a>
-
-        <div>
-            <button type="submit">
-                {{ __('Register') }}
-            </button>
-        </div>
-    </form>
+@section('custom_js')
+    {{-- Insert custom js scripts here. --}}
 @endsection
